7eee4270219a5c15de3309b786dd20ca150e4c54
youchan <youchan01@gmail.com>
2018-02-13 23:14:35 +0900

    Exchange row & col as method signatures, following how to record the kifu

--- lib/eighty_one/board.rb
+++ lib/eighty_one/board.rb
@@ -8,48 +8,48 @@ module EightyOne
 
     def initial_state
       (1..9).each do |i|
-        self[7, i] = Pieces::Fu.new(:sente)
-        self[3, i] = Pieces::Fu.new(:gote)
+        self[i, 7] = Pieces::Fu.new(:sente)
+        self[i, 3] = Pieces::Fu.new(:gote)
       end
       [1, 9].each do |i|
-        self[9, i] = Pieces::Ky.new(:sente)
-        self[1, i] = Pieces::Ky.new(:gote)
+        self[i, 9] = Pieces::Ky.new(:sente)
+        self[i, 1] = Pieces::Ky.new(:gote)
       end
       [2, 8].each do |i|
-        self[9, i] = Pieces::Ke.new(:sente)
-        self[1, i] = Pieces::Ke.new(:gote)
+        self[i, 9] = Pieces::Ke.new(:sente)
+        self[i, 1] = Pieces::Ke.new(:gote)
       end
       [3, 7].each do |i|
-        self[9, i] = Pieces::Gi.new(:sente)
-        self[1, i] = Pieces::Gi.new(:gote)
+        self[i, 9] = Pieces::Gi.new(:sente)
+        self[i, 1] = Pieces::Gi.new(:gote)
       end
       [4, 6].each do |i|
-        self[9, i] = Pieces::Ki.new(:sente)
-        self[1, i] = Pieces::Ki.new(:gote)
+        self[i, 9] = Pieces::Ki.new(:sente)
+        self[i, 1] = Pieces::Ki.new(:gote)
       end
-      self[8, 2] = Pieces::Ka.new(:sente)
-      self[2, 8] = Pieces::Ka.new(:gote)
+      self[2, 8] = Pieces::Ka.new(:sente)
+      self[8, 2] = Pieces::Ka.new(:gote)
       self[8, 8] = Pieces::Hi.new(:sente)
       self[2, 2] = Pieces::Hi.new(:gote)
-      self[9, 5] = Pieces::Ou.new(:sente)
-      self[1, 5] = Pieces::Ou.new(:gote)
+      self[5, 9] = Pieces::Ou.new(:sente)
+      self[5, 1] = Pieces::Ou.new(:gote)
     end
 
-    def inside?(row, col)
+    def inside?(col, row)
       1 <= row && 9 >= row && 1 <= col && 9 >= col
     end
 
-    def placeable?(piece, row, col)
-      inside?(row, col) && !(self[row, col]&.turn != piece.turn)
+    def placeable?(piece, col, row)
+      inside?(col, row) && !(self[col, row]&.turn != piece.turn)
     end
 
-    def []=(row, col, value)
-      assert(inside?(row, col))
+    def []=(col, row, value)
+      assert(inside?(col, row))
       assert(value.nil? || Piece === value)
       @board[(row - 1) * 9 + col - 1] = value
     end
 
-    def [](row, col)
+    def [](col, row)
       @board[(row - 1) * 9 + col - 1]
     end
 
@@ -59,22 +59,22 @@ module EightyOne
       @board[(row - 1) * 9, 9]
     end
 
-    def dests_from(row, col)
-      piece = self.at(row, col)
+    def dests_from(col, row)
+      piece = self.at(col, row)
       assert(Piece === piece)
-      piece.face.movements.select{|m| placeable?(m, row + m[0], col + m[1]) }
+      piece.face.movements.select{|m| placeable?(m, col + m[0], row + m[1]) }
     end
 
-    def move_from(row, col)
-      Transition.new(self, row, col)
+    def move_from(col, row)
+      Transition.new(self, col, row)
     end
 
-    def place(piece, row, col)
-      dest = self.at(row, col)
+    def place(piece, col, row)
+      dest = self.at(col, row)
       if dest
         dest.reset(piece.turn)
       end
-      self[row, col] = piece
+      self[col, row] = piece
     end
 
     def to_s
@@ -86,16 +86,16 @@ module EightyOne
     class Transition
       include Helper
 
-      def initialize(board, row, col)
-        @from = [row, col]
+      def initialize(board, col, row)
+        @from = [col, row]
         @board = board
-        @piece = board.at(row, col)
+        @piece = board.at(col, row)
         assert(Piece === @piece)
       end
 
-      def to(row, col)
+      def to(col, row)
         @board[*@from] = nil
-        @board.place(@piece, row, col)
+        @board.place(@piece, col, row)
       end
     end
   end
--- test/board_test.rb
+++ test/board_test.rb
@@ -5,10 +5,9 @@ class EightyOne::BoardTest < Test::Unit::TestCase
 
   def test_move
     @board.initial_state
-    @board.move_from(3,1).to(4,1)
+    @board.move_from(1,3).to(1,4)
 
-    puts @board.to_s
-    assert_equal(nil, @board[3,1])
-    assert_equal(:FU, @board[4,1].face.symbol)
+    assert_equal(nil, @board[1,3])
+    assert_equal(:FU, @board[1,4].face.symbol)
   end
 end
